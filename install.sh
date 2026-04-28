225ac0cb204e05a27960518bc3dc9ab32ea9524f
jhlee-wms <ji_hui.lee@samsung.com>
2026-04-28 14:53:08 +0900

    zsh theme

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
-cp ~/.dotfiles/gentoo.zsh-theme ~/.oh-my-zsh/themes/gentoo.zsh-theme
+# ~/.dotfiles/gentoo.zsh-theme ~/.oh-my-zsh/themes/gentoo.zsh-theme
